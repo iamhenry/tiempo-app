6f74e3679ce8df3d53c9e35c211db254195298b5
hmoran <hmoran@oath.com>
2020-04-22 09:25:04 -0700

    Cleaned up Primitives to include css SC

--- components/Global/Primitives.js
+++ components/Global/Primitives.js
@@ -2,39 +2,39 @@ import styled from "styled-components/native";
 import Tokens from "./Tokens";
 
 // TYPOGRAPHY
-export const H1 = styled.Text((props) => ({
-  color: props.color,
-  fontSize: `${Tokens.fontSize.h1}`,
-  fontWeight: props.fontWeight,
-  // add font family as prop for all
-}));
+// ADD FONT FAMILY PROP TO ALL
 
-export const H2 = styled.Text((props) => ({
-  color: props.color,
-  fontSize: `${Tokens.fontSize.h2}`,
-  fontWeight: props.fontWeight,
-}));
+export const H1 = styled.Text`
+  color: ${(props) => props.color || `${Tokens.color.blueMoon200}`};
+  font-size: ${Tokens.fontSize.h1};
+  font-weight: ${(props) => props.fontWeight || `${Tokens.fontWeight.regular}`};
+`;
 
-export const H3 = styled.Text((props) => ({
-  color: props.color,
-  fontSize: `${Tokens.fontSize.h3}`,
-  fontWeight: props.fontWeight,
-}));
+export const H2 = styled.Text`
+  color: ${(props) => props.color || `${Tokens.color.blueMoon200}`};
+  font-size: ${Tokens.fontSize.h2};
+  font-weight: ${(props) => props.fontWeight || `${Tokens.fontWeight.regular}`};
+`;
 
-export const H4 = styled.Text((props) => ({
-  color: props.color,
-  fontSize: `${Tokens.fontSize.h4}`,
-  fontWeight: props.fontWeight,
-}));
+export const H3 = styled.Text`
+  color: ${(props) => props.color || `${Tokens.color.blueMoon200}`};
+  font-size: ${Tokens.fontSize.h3};
+  font-weight: ${(props) => props.fontWeight || `${Tokens.fontWeight.regular}`};
+`;
 
-export const H5 = styled.Text((props) => ({
-  color: props.color,
-  fontSize: `${Tokens.fontSize.h5}`,
-  fontWeight: props.fontWeight,
-}));
+export const H4 = styled.Text`
+  color: ${(props) => props.color || `${Tokens.color.blueMoon200}`};
+  font-size: ${Tokens.fontSize.h4};
+  font-weight: ${(props) => props.fontWeight || `${Tokens.fontWeight.regular}`};
+`;
+
+export const H5 = styled.Text`
+  color: ${(props) => props.color || `${Tokens.color.blueMoon200}`};
+  font-size: ${Tokens.fontSize.h5};
+  font-weight: ${(props) => props.fontWeight || `${Tokens.fontWeight.regular}`};
+`;
 
 export const H6 = styled.Text`
-  /* REWRITE THE ABOVE STYLED COMPONENTS WITH THIS SYNTAX BELOW */
   color: ${(props) => props.color || `${Tokens.color.snowWhite100}`};
   font-weight: ${(props) => props.fontWeight || `${Tokens.fontWeight.regular}`};
   font-size: ${Tokens.fontSize.h6};
