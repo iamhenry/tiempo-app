bba6d87de7e4ca33f14e2148b418a6a14b89c816
hmoran <hmoran@oath.com>
2020-05-03 22:23:49 -0700

    Fixed missing react navigation package from Yarn

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Home from "./screens/Home/Home";
 import { TimerDetails } from "./screens/TimerDetails/TimerDetails";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
+import Navigator from "./routes/homeStack";
 
 const getFonts = () => {
   return Font.loadAsync({
@@ -21,7 +22,7 @@ export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   if (fontsLoaded) {
-    return <Home />;
+    return <Navigator />;
   } else {
     return (
       <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,11 +1,17 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import Home from "../screens/Home/Home";
+import { TimerDetails } from "../screens/TimerDetails/TimerDetails";
 
 const screens = {
   Home: {
     screen: Home,
   },
+  TimerDetails: {
+    screen: TimerDetails,
+  },
 };
 
 const HomeStack = createStackNavigator(screens);
+
+export default createAppContainer(HomeStack);
