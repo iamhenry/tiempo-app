b7d67a383c4c7fc11a062623e785475475fb387b
hmoran <hmoran@oath.com>
2020-07-10 09:35:05 -0700

    Add text styles to workout empty state

--- components/WorkoutList/WorkoutListEmptyState.js
+++ components/WorkoutList/WorkoutListEmptyState.js
@@ -1,14 +1,30 @@
 import React from "react";
 import styled from "styled-components/native";
+import { H5 } from "../Global/Primitives";
+import Tokens from "../Global/Tokens";
 
 const Container = styled.View``;
 
-const EmptyMessage = styled.Text``;
+const Title = styled(H5)`
+  color: ${Tokens.color.snowWhite100};
+  margin-bottom: ${Tokens.spacing.xSmall};
+  font-family: ${Tokens.fontFamily.nunitoBlack};
+  text-align: center;
+`;
+const Message = styled(H5)`
+  color: ${Tokens.color.snowWhite100};
+  font-family: ${Tokens.fontFamily.nunitoRegular};
+  text-align: center;
+`;
 
 export const WorkoutListEmptyState = () => {
   return (
     <Container>
-      <EmptyMessage>No workouts</EmptyMessage>
+      <Title>No Workouts</Title>
+      <Message>
+        There are no timers setup at the moment. Tap the button below to create
+        one.
+      </Message>
     </Container>
   );
 };
